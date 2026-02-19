da09d88006221588381d5848758e13d4d43e599b
meimo <59273253+meimodev@users.noreply.github.com>
2026-02-19 23:43:57 +0800

    Fix IconArrowRight to accept className

--- components/templates/neptune1/graphics/icons.tsx
+++ components/templates/neptune1/graphics/icons.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import type { SVGProps } from "react";
+
 export function IconEnvelope() {
     return (
         <svg width="18" height="18" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -14,9 +16,16 @@ export function IconEnvelope() {
     );
 }
 
-export function IconArrowRight() {
+export function IconArrowRight(props: SVGProps<SVGSVGElement>) {
     return (
-        <svg width="18" height="18" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <svg
+            width="18"
+            height="18"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            {...props}
+        >
             <path d="M9 18l6-6-6-6" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
         </svg>
     );
